b1089cb654dbf25a5bf7e619494472d3990e6bad
Martin <mckan91@gmail.com>
2017-10-03 11:57:23 +0200

    Removed check for android version, only building for api 19 or higher

--- app/src/main/java/com/dev/mckan/alarmclock/AlarmManagerHelper.java
+++ app/src/main/java/com/dev/mckan/alarmclock/AlarmManagerHelper.java
@@ -82,14 +82,11 @@ public class AlarmManagerHelper extends BroadcastReceiver {
 
     }
 
-    @SuppressLint("NewApi")
+    
     private static void setAlarm(Context context, Calendar calendar, PendingIntent pIntent) {
         AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
-            alarmManager.setExact(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pIntent);
-        } else {
-            alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pIntent);
-        }
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pIntent);
+
     }
 
     public static void cancelAlarms(Context context) {
